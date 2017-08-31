f687df497f3aab3aa6a64241bc0bdaa6b4f4e720
Ben Jackson <ben.d.jackson@outlook.com>
2017-08-31 13:18:20 +0100

    Added old content and started styling

--- kelvin/website/urls.py
+++ kelvin/website/urls.py
@@ -1,8 +1,26 @@
+
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
+    #home
     url(r'^$', views.home, name='home'),
+
+    #concerts
     url(r'^concerts$', views.concerts, name='concerts'),
+
+    #players
+    url(r'^info$', views.info, name='info'),
+    url(r'^join$', views.join, name='join'),
+    url(r'^calendar$', views.calendar, name='calendar'),
+
+    #about
+    url(r'^past-concerts$', views.pastConcerts, name='past-concerts'),
+    url(r'^history$', views.history, name='history'),
+    url(r'^gallery$', views.gallery, name='gallery'),
+    
+    #contact
+    url(r'^contact$', views.contact, name='contact'),
+        
 ]
--- kelvin/website/views.py
+++ kelvin/website/views.py
@@ -1,10 +1,38 @@
+
 from django.shortcuts import render
 
+#home
 def home(request):
-    print ("Home")
     return render(request, 'website/home.html')
 
+
+#concerts
 def concerts(request):
-    print ("Concerts")
     return render(request, 'website/concerts.html')
 
+
+#players
+def info(request):
+    return render(request, 'website/players/info.html')
+
+def join(request):
+    return render(request, 'website/players/join.html')
+
+def calendar(request):
+    return render(request, 'website/players/calendar.html')
+
+
+#about
+def pastConcerts(request):
+    return render(request, 'website/about/past-concerts.html')
+
+def history(request):
+    return render(request, 'website/about/history.html')
+
+def gallery(request):
+    return render(request, 'website/about/gallery.html')
+
+
+#contact
+def contact(request):
+    return render(request, 'website/contact.html')
