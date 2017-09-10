ffb6526ea55d70b4600ca06db66a89de439e8786
Ben Jackson <ben.d.jackson@outlook.com>
2017-09-10 22:32:20 +0100

    Removed magnific pop-up

--- kelvin/website/static/website/vendor/creative/creative.js
+++ kelvin/website/static/website/vendor/creative/creative.js
@@ -52,20 +52,4 @@
     distance: '0px'
   }, 300);
 
-  // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
-
 })(jQuery); // End of use strict
