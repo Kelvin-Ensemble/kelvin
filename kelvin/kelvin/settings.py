a6c6e244696037fbdf3c70277dddc04203e1b6bc
YukiSuter <yukisuter@ycsuter.dev>
2023-08-26 20:14:13 +0100

    dotenv requirement

--- kelvin/kelvin/settings.py
+++ kelvin/kelvin/settings.py
@@ -41,7 +41,6 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-
 from dotenv import load_dotenv
 
 dotenv_file = os.path.join(BASE_DIR, ".env")
