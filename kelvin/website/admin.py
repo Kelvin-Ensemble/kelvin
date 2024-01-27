b8ba32ce583653da09631f3a79fda0c0a2f0b774
YukiSuter <yukisuter@ycsuter.dev>
2024-01-27 15:14:09 +0000

    Updated the models with more

--- kelvin/website/admin.py
+++ kelvin/website/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Ticket
 from .models import Concert
-from .models import Ticket_Type
+from .models import TicketType
 
 # Register your models here.
 
@@ -9,4 +9,4 @@ admin.site.site_header = "Kelvin Ensemble Web Admin"
 admin.site.site_title = "Kelvin Ensemble Web Admin"
 
 admin.site.register(Concert)
-admin.site.register(Ticket_Type)
+admin.site.register(TicketType)
--- kelvin/website/models.py
+++ kelvin/website/models.py
@@ -11,15 +11,40 @@ class Concert(models.Model):
         ("SO", "Sold Out"),
         ("NY", "Not Yet"),
     )
+    Concert_Nickname = models.CharField(
+        max_length=15,
+        help_text="This is just a nickname for this concert to make it easier for you to distinguish in the admin page.",
+        null=False,
+        blank=False,
+        default="Concert",
+    )
     Concert_Status = models.CharField(max_length=2, choices=status_choices)
     Concert_Date = models.DateTimeField(default=datetime.now, blank=True)
 
+    def __str__(self):
+        return self.Concert_Nickname
 
-class Ticket_Type(models.Model):
-    ticket_label = models.CharField(max_length=40)
-    Ticket_ID = models.CharField(max_length=40)
-    Linked_Tickets = models.ForeignKey(
-        "Ticket_Type", on_delete=models.CASCADE, blank=True, null=True
+
+class TicketType(models.Model):
+    ticket_label = models.CharField(
+        max_length=40,
+        help_text="This will be the ticket name shown to the audience (i.e. 'Standard Seating' or 'Concession Seating' or 'Restricted View')",
+    )
+    for_concert = models.OneToOneField(
+        Concert,
+        help_text="Please select the matching concert here. Make sure this is selected or the ticket will not show.",
+        on_delete=models.CASCADE,
+        default=None,
+        null=True,
+    )
+    Ticket_ID = models.CharField(
+        max_length=40,
+        help_text="This will be the price ID supplied by STRIPE which you can obtain via the treasurer. Refer to the webmaster bible's payments section for more information",
+    )
+    Linked_Tickets = models.ManyToManyField(
+        "TicketType",
+        blank=True,
+        help_text="This should auto-populate when saving as long as the STRIPE products and prices are set up correctly. If this does not happen it may be done manually here. However it is not recommended.",
     )
 
     def __str__(self):
