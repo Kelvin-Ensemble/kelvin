ca387e66c5fd74a43a7790e89bc948546c89a20d
YukiSuter <yukisuter@ycsuter.dev>
2024-01-27 13:33:11 +0000

    Fixed model naming

--- kelvin/website/models.py
+++ kelvin/website/models.py
@@ -15,9 +15,11 @@ class Concert(models.Model):
 
 
 class Ticket_Type(models.Model):
-    name = models.CharField(max_length=40)
+    ticket_label = models.CharField(max_length=40)
     Ticket_ID = models.CharField(max_length=40)
     Linked_Tickets = models.ForeignKey("Ticket_Type", blank=True, null=True)
+    def __str__(self):
+        return self.ticket_label
 
 class Ticket(models.Model):
     name = models.CharField(max_length=60)
